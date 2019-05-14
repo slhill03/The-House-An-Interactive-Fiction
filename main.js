3fff9369abc4e3bc563fe6836f5e5ca82c32cdab
Sam H <47217896+slhill03@users.noreply.github.com>
2019-05-14 14:29:25 -0400

    Add files via upload
    added new descriptions to rooms and items

--- main.js
+++ main.js
@@ -1,25 +1,23 @@
 // contents of room here
-alert("Loading main.js!"); //Don't change this line
+alert("Loading main.js!");
 player = new Player();
 
-//Create your objects
-let room1 = new Room("lab 1");
+//objects
+let room1 = new Room("lab 1" , "Lab that produces highly dangerous substances.");
 let testtubes = new Item("testing tubes", "very cloudy and has illegible writing. Filled with a dark substance.")
-let monster = new Item("monster")
-let room2 = new Room("lab 2");
+let room2 = new Room("lab 2" , "Wires are everywhere connecting to some kind of pod.");
 let pods = new Item("pod" , "very cold and contains a very hairy figure inside.")
-let room3 = new Room("security room");
-let cctv = new Item("cctv cameras")
-let room4 = new Room("elevator");
-let buttons = new Item("elevator buttons")
+let room3 = new Room("security room" , "Has camera moniters and a locked arsenal.");
+let cctv = new Item("cctv cameras" , "Shows the dark outside world and all the rooms around the Lab.")
+let room4 = new Room("elevator" , "An out of order elevator");
+let buttons = new Item("elevator buttons" , "The buttons won't work...")
 let room5 = new Room("analysis room");
-let results = new Item("lab results")
+let results = new Item("lab results" , "Mr. Whiskers, Age: 4 (in cat years), Highly Dangerous")
 let start = new Room("Testing Lab", "You have no memory of this place.");
 
 
-// Put them in their spots
+// spots
 room1.addItem(testtubes);
-room1.addItem(monster);
 room2.addItem(pods);
 room3.addItem(cctv);
 room4.addItem(buttons);
