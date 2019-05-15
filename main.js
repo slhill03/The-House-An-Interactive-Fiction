86a838c9d926bdf8bffe1ac7a745450b41bb3868
Sam H <47217896+slhill03@users.noreply.github.com>
2019-05-15 11:58:00 -0400

    Add files via upload

--- main.js
+++ main.js
@@ -30,4 +30,38 @@ start.addItem(room5);
 player.location = start;
 
 
-player.location.enter();
+player.move(start);
+
+
+
+// Create the function
+let jumpaction = /jump/;
+let jump = function(action, player, object) {
+  if (action == 'jump') {
+    addLine('You jump. As you land, you hear a loud creak from the floor.');
+  }
+  return player
+}
+
+//To create a function that interacts with an object...
+let eatAction = /eat/;
+let eat = function(testingtubes) {
+  if (action == 'eat') {
+    object.eat();
+  }
+  return player;
+}
+
+//... then add a method to an object. This only works for the one object.
+testingtubes.eat = function() {
+  addLine("You ate the " + this.name)
+}
+
+// You can also extend the class, adding an eat() method to all items in your game.
+Item.prototype.eat = function () {
+  addLine("You ate the " + this.name);
+}
+
+//Add the function to the action list
+addAction(jumpaction, jump);
+addAction(eatAction, eat);
